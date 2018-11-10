bc32fcc97cd57262959ff4709960568f5b03b348
h1sonath <41195975+h1sonath@users.noreply.github.com>
2018-11-11 03:42:19 +0700

    Update commentModel.js

--- Techkids/models/commentModel.js
+++ Techkids/models/commentModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const CommentSchema = new Schema({
-    user: { type: String},
-    content: { type: String, required: true}
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, required: true }
 });
-module.exports = mongoose.model("Comment", CommentSchema);
+
+const CommentModel = mongoose.model("Comment", CommentSchema);
+
+module.exports = CommentModel;
