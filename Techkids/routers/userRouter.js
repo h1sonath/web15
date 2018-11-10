f8f3cf4bbfe57d69ed677e7c90cb257f72fc7d0c
h1sonath <41195975+h1sonath@users.noreply.github.com>
2018-11-11 03:39:28 +0700

    Update userRouter.js

--- Techkids/routers/userRouter.js
+++ Techkids/routers/userRouter.js
@@ -1,68 +1,94 @@
 const express = require('express');
 const UserRouter = express.Router();
+const UserModel = require('../models/userModel');
 
-const UserModel = require('../models/userModel')
-UserRouter.use((req, res, next) => {
-    console.log("User middleware");
-    next();
+// UserRouter.use((req, res, next) => {
+//     console.log("User middleware");
+//     next();
+// });
+
+UserRouter.get("/", async (req, res) => {
+    try {
+        const users = await UserModel.find({}, "name email avatar intro posts").populate("posts");
+        res.json({ success: 1, users })
+    }
+    catch{
+        res.status(500).json({ success: 0, error: error })
+    }
+
+    // .then(users => res.json({ success: 1, users }))
+    // .catch(err => res.status(500).json({ success: 0, error: err }))
 });
 
-UserRouter.get("/", (req, res) => {
-    UserModel.find({},"-password", (err, users) => {
-        if (err) res.status(500).json({ success: 0, error: err })
-        else res.json({ success: 1, users });
-    })
-});
-
-UserRouter.get("/:id", (req, res) => {
+UserRouter.get("/:id", async (req, res) => {
     let userId = req.params.id;
-    UserModel.findById(userId, (err, userFound) => {
-        if(err) res.status(500).json({ success: 0, message: err })
-        else if(!userFound._id) res.status(404).json({ success: 0, message: "Not Found"})
-        else res.json({ success: 1, user: userFound})
-    })
-});
-
-UserRouter.post("/", (req, res) => {
-    const { name, email, password, avatar, intro } = req.body;
-    UserModel.create({ name, email, password, avatar, intro }, (err, userCreated) => {
-        if(err) res.status(500).json({ success:0, message: err})
-        else res.status(201).json({ success: 1, user: userCreated})
-    })
-});
-
-UserRouter.put("/:id", (req, res) =>{
-    let userId = req.params.id;
-    const { name, password, avatar, intro} = req.body;
-    // UserModel.findByIdAndUpdate(userId, {name ,password, avatar, intro},{new: true}, (req, userUpdated) =>{
-    //     if(err) res.status(500).json({ success:0, message: err})
-    //     else res.status(201).json({ success: 1, user: userUpdated})
-    // })
-
-    UserModel.findById(userId, (err, userFound)=>{
-        if(err) res.status(500).json({ success: 0, message: err })
-        else if(!userFound._id) res.status(404).json({ success: 0, message: "Not Found"})
-        // else userFound.name = name || userFound.name;
-        //      userFound.password = password || userFound.password;
-        //      userFound.intro = intro || userFound.intro;
-        //      userFound.avatar = avatar || userFound.avatar;
+    try {
+        const userFound = await UserModel.findById(userId).populate("posts");
+        if (!userFound) res.status(404).json({ success: 0, message: "Not Found" });
         else {
-            for(key in { name, password, avatar, intro }) {
-                if(userFound[key] && req.body[key]) userFound[key] = req.body[key];
-            }
+            res.json({ success: 1, user: userFound });
         }
-        userFound.save((err, userUpdated)=>{
-            if(err) res.status(500).json({ success:0, message: err})
-            else res.status(201).json({ success: 1, user: userUpdated})
-        })
-    })
-})
+    }
+    catch (error) {
+        res.status(500).json({ success: 0, error: error });
+    }
+});
 
-    UserRouter.delete("/:id", (req, res) =>{
-        const userId = req.params.id;
-        UserModel.remove(userId, (err)=>{
-            if (err) res.status(500).json({success: 0, message: err})
-            else res.json({success: 1})
-        })
-    })
+UserRouter.post("/", async (req, res) => {
+    const { name, email, password, avatar, intro } = req.body;
+    console.log(req.body);
+
+    try {
+        const userCreated = await UserModel.create({ name, email, password, avatar, intro });
+        res.status(201).json({ success: 1, user: userCreated })
+    }
+
+    catch (err) {
+        res.status(500).json({ success: 0, message: err });
+    }
+});
+
+UserRouter.put("/:id", async (req, res) => {
+    let userId = req.params.id;
+    const { name, password, avatar, intro, posts } = req.body;
+    // UserModel.findById(userId, (err, userFound) => {
+    //     if (err) res.status(500).json({ success: 0, message: err })
+    //     else if (!userFound) res.status(404).json({ success: 0, message: "Not Found" })
+    //     else {
+    //         for(key in { name, password, avatar, intro, posts }) {
+    //             if(userFound[key] && req.body[key]) userFound[key] = req.body[key];
+    //         }
+    //         userFound.save((err, userUpdated) => {
+    //             if (err) res.status(500).json({ success: 0, message: err })
+    //             else res.status(201).json({ success: 1, user: userUpdated })
+    //         })
+    //     }
+    // })
+    try {
+        const userFound = await UserModel.findById(userId);
+        if (!userFound) {
+            res.status(404).json({ success: 0, message: "Not found" });
+        } else {
+            for (key in { name, password, avatar, intro, posts }) {
+                if (userFound[key] && req.body[key]) userFound[key] = req.body[key];
+            }
+            let userUpdated = await userFound.save();
+            res.json({ success: 1, user: userUpdated });
+        }
+    } catch (error) {
+        res.status(500).json({ success: 0, message: error });
+    }
+});
+
+UserRouter.delete("/:id", async (req, res) => {
+    let userId = req.params.id;
+    try {
+        await UserModel.findByIdAndRemove(userId);
+        res.json({ success: 1 });
+    }
+    catch (err) {
+        res.status(500).json({ success: 0, message: err })
+    }
+});
+
 module.exports = UserRouter;
